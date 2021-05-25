30477f76828e3116e4ac69c2868374afce22165b
Cody Shepherd <cody.shepherd@gmail.com>
2021-05-25 14:12:34 -0700

    WIP: produce packages from all parallel test runs

--- test.sh
+++ test.sh
@@ -108,18 +108,18 @@ case $ENVIRONMENT_INDEX in
     1)
         # 64 bit race tests
         GORACE="halt_on_error=1"
-        run_test_docker Dockerfile_build_ubuntu64 test_64bit_race --test --junit-report --race
+        run_test_docker Dockerfile_build_ubuntu64 test_64bit_race --test --junit-report --race --package
         rc=$?
         ;;
     2)
         # 32 bit tests
-        run_test_docker Dockerfile_build_ubuntu32 test_32bit --test --junit-report --arch=i386
+        run_test_docker Dockerfile_build_ubuntu32 test_32bit --test --junit-report --arch=i386 --package
         rc=$?
         ;;
     3)
         # tsi
         INFLUXDB_DATA_INDEX_VERSION="tsi1"
-        run_test_docker Dockerfile_build_ubuntu64 test_64bit --test --junit-report
+        run_test_docker Dockerfile_build_ubuntu64 test_64bit --test --junit-report --package
         rc=$?
         ;;
     "count")
